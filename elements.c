00b2f9c778b8dda68561f2f2d5f1271ba4c2e50a
JoyNgaru <joywanjikum@gmail.com>
2023-04-24 15:28:21 -0700

    update

--- elements.c
+++ elements.c
@@ -1,76 +1,51 @@
 #include "main.h"
+#include <stdarg.h>
+
 /**
- * print_number - function to print nums
- * print_unsgined_number - print unsigned num
- * @args: arguments passed
- * @n: the number
- * Return: int
- */
-int print_unsgined_number(unsigned int n)
-{
-        int check;
-        int len;
-        unsigned int num;
-
-        check = 1;
-        len = 0;
-
-        num = n;
-
-        for (; num / check > 9; )
-                check *= 10;
-
-        for (; check != 0; )
-        {
-                len += _putchar('0' + num / check);
-                num %= check;
-                check /= 10;
-        }
-
-        return (len);
-}
-
+* print_number - function to print nums
+* print_unsgined_number - print unsigned num
+* @args: arguments passed
+* @n: the number
+* @len: length
+* @num: num
+* Return: int
+*/
 int print_number(va_list args)
 {
-	int n;
-	int check;
-	int len;
-	unsigned int num;
+int n = va_arg(args, int);
+int check = 1;
+int len = 0;
+unsigned int num;
 
-	n  = va_arg(args, int);
-	check = 1;
-	len = 0;
+if (n < 0)
+{
+len += _putchar('-');
+num = (unsigned int)(-1 * n); /* Cast to unsigned int to get absolute value*/
+}
+else
+{
+num = (unsigned int)n; /* Cast to unsigned int to handle unsigned numbers*/
+}
 
-	if (n < 0)
-	{
-		len += _putchar('-');
-		num = n * -1;
-	}
-	else
-		num = n;
-
-	for (; num / check > 9; )
-		check *= 10;
-
-	for (; check != 0; )
-	{
-		len += _putchar('0' + num / check);
-		num %= check;
-		check /= 10;
-	}
-
-	return (len);
+for (; num / check > 9; )
+{
+check *= 10;
+}
+for (; check != 0; )
+{
+len += _putchar('0' + num / check);
+num %= check;
+check /= 10;
+}
+return (len);
 }
 
 
-
-
-
 /**
- * p_char - fun to print chars
- * @args: arguments passed in the function
- * Return: printed value
- */
+* p_char - fun to print chars
+* @args: arguments passed in the function
+* Return: printed value
+*/
 
 
 int p_char(va_list args)
@@ -83,10 +58,10 @@ int p_char(va_list args)
 }
 
 /**
- * p_string - func to print string
- * @args: arguments
- * Return: printed character
- */
+* p_string - func to print string
+* @args: arguments
+* Return: printed character
+*/
 
 int p_string(va_list args)
 {
@@ -102,10 +77,10 @@ int p_string(va_list args)
 }
 
 /**
- * p_percent - fun to print %
- * @args: args
- * Return: printed character
- */
+* p_percent - fun to print %
+* @args: args
+* Return: printed character
+*/
 
 int p_percent(__attribute__((unused)) va_list args)
 {
@@ -115,10 +90,10 @@ int p_percent(__attribute__((unused)) va_list args)
 
 
 /**
- * p_integer - fun to print int
- * @args: args
- * Return: printed character
- */
+* p_integer - fun to print int
+* @args: args
+* Return: printed character
+*/
 int p_integer(va_list args)
 {
 	int n;

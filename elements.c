3d4f3287c85536632954402483657bc7a91379b0
JoyNgaru <joywanjikum@gmail.com>
2023-04-24 11:08:33 -0700

    update

--- elements.c
+++ elements.c
@@ -39,34 +39,7 @@ int print_number(va_list args)
 }
 
 
-/**
- * print_unsgined_number - print unsigned num
- * @n: the number
- * Return: len
- */
-int print_unsgined_number(unsigned int n)
-{
-	int check;
-	int len;
-	unsigned int num;
 
-	check = 1;
-	len = 0;
-
-	num = n;
-
-	for (; num / check > 9; )
-		check *= 10;
-
-	for (; check != 0; )
-	{
-		len += _putchar('0' + num / check);
-		num %= check;
-		check /= 10;
-	}
-
-	return (len);
-}
 
 
 /**

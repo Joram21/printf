81a97a28398039e56d2bc3146bd4a601943a41e4
JoyNgaru <joywanjikum@gmail.com>
2023-04-24 04:21:02 -0700

    update

--- elements.c
+++ elements.c
@@ -1,4 +1,44 @@
 #include "main.h"
+
+/**
+ * print_number - function to print nums
+ * @args: arguments passed
+ * Return: 0(success)
+ */
+
+int print_number(va_list args)
+{
+	int n;
+	int check;
+	int len;
+	unsigned int num;
+
+	n  = va_arg(args, int);
+	check = 1;
+	len = 0;
+
+	if (n < 0)
+	{
+		len += _putchar('-');
+		num = n * -1;
+	}
+	else
+		num = n;
+
+	for (; num / check > 9; )
+		check *= 10;
+
+	for (; check != 0; )
+	{
+		len += _putchar('0' + num / check);
+		num %= check;
+		check /= 10;
+	}
+
+	return (len);
+}
+
+
 /**
  * print_unsgined_number - print unsigned num
  * @n: the number

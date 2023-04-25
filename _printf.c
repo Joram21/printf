69ca5df17800b5f3cf97c620abdbb25e422d9f96
JoyNgaru <joywanjikum@gmail.com>
2023-04-24 23:08:06 -0700

    update

--- _printf.c
+++ _printf.c
@@ -1,37 +1,73 @@
+#include <stdio.h>
+#include <stdarg.h>
 #include "main.h"
-#include <stdint.h>
 
 /**
- * _printf - funtion to print
- * @format: format
- * Return: printed chars
- */
-
+* _printf - function to print
+*  @format: type
+*  ...: unkown args
+*  Return: printed char
+*/
 int _printf(const char *format, ...)
 {
-	int printed_chars;
+int count = 0;
+va_list arg_list;
+va_start(arg_list, format);
 
-	conver_t funct_list[] = {
-		{"c", p_char},
-		{"s", p_string},
-		{"%", p_percent},
-		{"d", p_integer},
-		{"i", p_integer},
-		{NULL, NULL}
-	};
-
-	va_list args;
-
-
-	if (format == NULL)
-		return (-1);
-
-	va_start(args, format);
-
-	/** call a parser function*/
-
-	printed_chars = parser(format, funct_list, args);
-	va_end(args);
-
-	return (printed_chars);
+while (*format)
+{
+if (*format == '%')
+{
+format++;
+switch (*format)
+{
+case 'c':
+putchar(va_arg(arg_list, int));
+count++;
+break;
+case 's':
+count += printf("%s", va_arg(arg_list, char *));
+break;
+case '%':
+putchar('%');
+count++;
+break;
+default:
+/* unsupported format specifier*/
+putchar('%');
+putchar(*format);
+count += 2;
+break;
 }
+}
+else
+{
+putchar(*format);
+count++;
+}
+format++;
+}
+va_end(arg_list);
+return (count);
+}
+
+
+/* task2*/
+
+
+/**
+ * print_binary - print funct
+ * @num: nmber
+ * @buffer: size
+ * @count: count
+ * Return: binaru
+ */
+void print_binary(unsigned int num, char *buffer, int *count)
+{
+    if (num > 1) {
+        print_binary(num / 2, buffer, count);
+    }
+    buffer[*count % 1024] = num % 2 + '0';
+    (*count)++;
+}
+
